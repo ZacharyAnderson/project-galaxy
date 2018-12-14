91aba2b127a868a39b04e97aa561b03bfb2e0497
Zach Anderson <zachanderson@111.sub-97-32-78.myvzw.com>
2018-12-14 10:38:24 -0500

    Added react-router-dom to the index for the ability to route to different pages
     Author:    ZacharyAnderson <zacha777@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,22 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+const routing = (
+  <Router>
+    <div>
+      <Route path="/" component={App} />
+    </div>
+  </Router>
+);
+
+
 ReactDOM.render(
-  <App />,
+  routing,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
