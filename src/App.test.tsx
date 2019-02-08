f99ed8fdc0573b1ff6813558ccbdf2b6c3912f4f
ZacharyAnderson <zacha777@gmail.com>
2019-02-08 15:08:59 -0500

    Added testing functionality for unit tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,11 @@
 import { shallow } from "enzyme";
 import * as React from "react";
+import "../setupTests";
 import App from "./App";
 
 describe("<App />", () => {
   it("renders 1 <App /> component", () => {
     const component = shallow(<App />);
-    expect(component).toHaveLength(1);
+    expect(component).toMatchSnapshot();
   });
 });
